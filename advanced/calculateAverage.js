845281a403aa76d7384f50d179e32d1eea22ea70
Aminmohammad0709 <76067355+Aminmohammad0709@users.noreply.github.com>
2025-01-24 18:24:21 +0330

    solved calculateAverage

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -10,4 +10,33 @@
  * calculateAverage([10, 20, 30]) should return 20.
  */
 
+function calculateAverage(...arg) {
+
+
+    let arr = []
+    let sumArray = 0
+    for (let number of arg) {
+
+        let x = number
+
+        arr.push(x)
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        sumArray = sumArray + arr[i]
+
+    }
+
+
+    let resultAverage = sumArray / arr.length
+
+    console.log(resultAverage);
+
+
+
+}
+
+calculateAverage(1, 2, 3, 4)
+
+
 module.exports = calculateAverage;
