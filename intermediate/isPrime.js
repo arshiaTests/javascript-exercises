68607b84a97159fa29e2599a824e1deb294c2921
Aminmohammad0709 <76067355+Aminmohammad0709@users.noreply.github.com>
2025-01-24 17:49:51 +0330

    solved isPrime

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -12,4 +12,31 @@
 
 // Write your solution here
 
+function isPrime(n, x = 3) {
+    if (n < 2) {
+        return false;
+    }
+
+    while (n >= 2) {
+
+        if (n === 2) {
+            return true;
+        }
+
+        if (n > 2 && n % 2 === 0) return false;
+
+
+        if (x > Math.sqrt(n)) return true;
+
+        if (n % 3 === 0) return false;
+
+
+    }
+
+
+}
+
+
+console.log(isPrime(7));
+
 module.exports = isPrime;
