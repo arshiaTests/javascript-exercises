d7ba88872a2a6bc64abe8d163157d93beccc53ae
Aminmohammad0709 <76067355+Aminmohammad0709@users.noreply.github.com>
2025-02-07 19:52:44 +0330

    solved userManagement

--- advanced/userManagement.js
+++ advanced/userManagement.js
@@ -14,4 +14,35 @@
  * showUsers() should return [].
  */
 
+let userList = ['Alice', 'Bob', 'Charlie'];
+
+function addUser(newUser) {
+    if (typeof newUser === 'number') {
+        console.log('This is a number, not a valid username!');
+    } else {
+        userList.push(newUser);
+    }
+    console.log(userList);
+}
+
+function removeUser(user) {
+    let indexOfUser = userList.indexOf(user);
+
+    if (indexOfUser !== -1) {
+        userList.splice(indexOfUser, 1);
+    } else {
+        console.log('User not found!');
+    }
+
+    console.log(userList);
+}
+
+function showUsers() {
+    console.log(userList);
+}
+
+addUser('David');
+removeUser('Bob');
+showUsers();
+
 module.exports = { addUser, removeUser, showUsers };
